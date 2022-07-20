cefb3199e5b56ed89f680de793ecc4c88acc4016
lspiovezan <91438025+lspiovezan@users.noreply.github.com>
2022-07-20 16:03:35 -0300

    Add files via upload

---
+++ Desafio-Banco-Santander-Java/README.md
@@ -0,0 +1,3 @@
+# Banco-Dio
+desafio do bootcamp Santander java full stack
+Implementação simples de um banco em java, utilizando os conceitos de OO. 
---
+++ Desafio-Banco-Santander-Java/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente antonio = new Cliente();
+		antonio.setNome("Antonio");
+		
+		
+		Conta cc = new ContaCorrente(antonio);
+		Conta poupanca = new ContaPoupanca(antonio);
+		
+		
+		cc.depositar(150);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+	}
+
+}
